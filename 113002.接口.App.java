6ffd53db13611a7d5a3bc62b3328af3beed9a84d
lil-ni <74757936+lil-ni@users.noreply.github.com>
2020-11-30 15:42:00 +0800

    113002.接口.App.java

---
+++ 113002.接口.App.java
@@ -0,0 +1,17 @@
+package pic;
+
+public class App implements R {
+public static void main(String[] args) {
+	int [][]array2d={
+			{1,2},
+			{3,4},
+			{5,6}
+	};
+	for (int row = 0; row < array2d.length;row++){
+	for (int column = 0; column < array2d[0].length; column++){
+		System.out.print(array2d[row][column]+"");
+	}
+	System.out.println();
+	}
+}	
+}
