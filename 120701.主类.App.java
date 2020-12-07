bd17528e60462e0a657ce4963161d27a2afcddfb
lil-ni <74757936+lil-ni@users.noreply.github.com>
2020-12-07 15:48:41 +0800

    120701.主类.App.java

---
+++ 120701.主类.App.java
@@ -0,0 +1,18 @@
+package pii;
+
+public class App {
+	public static void main(String[] args) {
+	 	int array2d[ ][ ]={
+		{1,2,3,4,5,6},
+		{7,8,9,10,0,12},
+		{8,8,9,10,11,12},
+		{7,8,3,10,11,12},
+		{88,66,92,102,151,999}
+		};
+	    new Find().findZero(array2d);
+	    new Find().findZero1(array2d);
+	    new Find().findZero2(array2d);
+	    new Find().findZero3(array2d);
+	    new Find().findZero4(array2d);
+	}
+	}
